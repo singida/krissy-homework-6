fe0adb76ef2ad4b52de2ade4aa9a4336945cd867
Joni Halabi <her@thatdevgirl.com>
2016-04-26 09:47:30 -0400

    Feedback on homework 6

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
+/*
+    Hi Krissy!  Your validation function here looks great!  No real comments here, other than
+    what I said in the HTML file, that you can use this function to also redirect the page
+    to your thank you page - however, keep in mind that you only want people to go to the 
+    thank you page if the validation you have passes.   There's an easy way to do this - I added
+    that code inline.
+*/
+
 $(document).ready(function() {
 
 $('form').submit(validateForm);
@@ -7,12 +15,17 @@ function validateForm() {
 
 	var firstName = $('#firstName').val();
 	if (firstName == '') {
-		alert('You must enter your first name')
+		alert('You must enter your first name');
+        return false; // Tells the this function to stop runnning, which you want since the user made a mistake.
 	}
 	var lastName = $('#lastName').val();
 	if (lastName == '') {
-		alert('You must enter your last name')
+		alert('You must enter your last name');
+        return false; // Tells the this function to stop runnning, which you want since the user made a mistake.
 	}
+
+    // The user will only get to this point if they did not make a mistake, so now you can redirect to the thank you page.
+    window.location.assign('thanks.html');
 }
 
 
